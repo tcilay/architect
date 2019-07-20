caebe06db32fa844a58bd303e8809bdd39393960
tcilay <tcilay@aliyun.com>
2019-07-20 21:37:08 +0800

    master:optimization test and compiler version

--- src/test/java/com/tcilay/architect/ArchitectApplicationTests.java
+++ src/test/java/com/tcilay/architect/ArchitectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.tcilay.architect;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,4 +12,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(classes = ArchitectApplication.class)
 @RunWith(SpringRunner.class)
 public class ArchitectApplicationTests {
+
+    @Test
+    public void testOne(){
+        System.out.println("test hello world");
+    }
 }
